0d4cf5bba203e01ad67fae984a03b1e97ff98491
Pierre Joye <pierre.php@gmail.com>
2015-03-19 14:11:21 +0700

    test gen #2

---
+++ publish.sh
@@ -0,0 +1,19 @@
+if [ $# -ne 1 ]; then
+echo "usage: ./publish.sh \"commit message\""
+exit 1;
+fi
+
+# sculpin generate --env=prod
+
+git stash
+git checkout master
+
+cp -R output_prod/* .
+rm -rf output_*
+
+git add *
+git commit -m "$1"
+git push origin master 
+
+git checkout gh-pages 
+git stash pop
