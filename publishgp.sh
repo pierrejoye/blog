a51f605f6a010cbd760d47ced5d0848d8e706f78
Pierre Joye <pierre.php@gmail.com>
2015-03-19 17:14:38 +0700

    let get it work

--- publishgp.sh
+++ publishgp.sh
@@ -3,7 +3,6 @@ echo "usage: ./publish.sh \"commit message\""
 exit 1;
 fi
 
-git stash
 git checkout master
 
 cp -R output_prod/* .
@@ -11,7 +10,6 @@ rm -rf output_*
 
 git add *
 git commit -m "$1"
-git push origin --all
+git push origin master 
 
 git checkout gh-pages
-git stash pop
